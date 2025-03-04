7adfc859346e29629a1569c76e9f5cf9fcaf3a88
454 <g920430@gmail.com>
2025-03-04 15:26:12 +0800

    將衝突協程的enemy來源更改
    將衝突協程的enemy來源從battleEnemyController.InstanceEnemyObject改成battleGameController.InstancedEnemy

--- Assets/454/Script/Battle/BattleConfrontation/BattleConfrontationController.cs
+++ Assets/454/Script/Battle/BattleConfrontation/BattleConfrontationController.cs
@@ -92,14 +92,14 @@ public class BattleConfrontationController : MonoBehaviour
             //¶}©l½Ä¬ð
             confrontationAnimationController.Start_Confrontation(
                 nowBattlePartner,
-                battleEnemyController.InstanceEnemyObject[0].GetComponent<Enemy>(),
+                battleGameController.InstancedEnemy[0],
                 partnerAttack,
                 EnemyAttack
                 );
 
             yield return StartCoroutine(confrontationAnimationController.Start_Confrontation(
                 nowBattlePartner,
-                battleEnemyController.InstanceEnemyObject[0].GetComponent<Enemy>(),
+                battleGameController.InstancedEnemy[0],
                 partnerAttack,
                 EnemyAttack
                 ));
