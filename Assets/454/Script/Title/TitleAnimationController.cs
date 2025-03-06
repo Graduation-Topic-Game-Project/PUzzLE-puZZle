fbead475a6c72de6716b3a4c9811f0dc3bc21476
454 <g920430@gmail.com>
2025-03-06 18:20:44 +0800

    標題完成

---
+++ Assets/454/Script/Title/TitleAnimationController.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class TitleAnimationController : MonoBehaviour
+{
+    public VideoClip Title_In; // 動態標題入
+    public VideoClip Title_Cycle; // 動態標題循環
+
+    private VideoPlayer videoPlayer;
+    private bool isFirstVideoPlaying = true;
+
+    void Start()
+    {
+        videoPlayer = GetComponent<VideoPlayer>();
+
+        // 綁定事件
+        videoPlayer.loopPointReached += OnVideoEnd;
+    }
+    void OnVideoEnd(VideoPlayer vp) //影片結束時執行
+    {
+
+        if (isFirstVideoPlaying)
+        {
+            Debug.Log("Title in 撥放結束");
+            isFirstVideoPlaying = false;
+            PlayVideo(Title_Cycle);
+        }
+    }
+    void PlayVideo(VideoClip clip)
+    {
+        videoPlayer.clip = clip;
+        videoPlayer.Play();
+    }
+
+    private void OnDestroy()
+    {
+        videoPlayer.loopPointReached -= OnVideoEnd;
+    }
+}
+
