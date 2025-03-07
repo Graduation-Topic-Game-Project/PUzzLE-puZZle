9d57db52430cc028ab0129aa2adae1ae8f343695
454 <g920430@gmail.com>
2025-03-07 20:54:56 +0800

    修正畫面外背景

--- Assets/454/Script/Battle/Enemy/BattleEnemyController.cs
+++ Assets/454/Script/Battle/Enemy/BattleEnemyController.cs
@@ -12,7 +12,7 @@ public class BattleEnemyController : MonoBehaviour
     public GameObject[] EnemyPositionGameObject = new GameObject[3]; //依照敵方總數來選擇生成位置
     public EnemyBattleUI[] enemyBattleUI = new EnemyBattleUI[3];  //右方敵方資訊
     [Tooltip("是否顯示右方敵方資訊UI")]
-    public bool isEnemyBattleUI; //是否顯示右方UI
+    public bool openEnemyBattleUI; //是否顯示右方UI
     GameObject NowEnemyPosition;
     public int EnemiesNumber; //敵方總數
 
@@ -87,7 +87,7 @@ public class BattleEnemyController : MonoBehaviour
     /// <param name="EnemyAmount">敵人數量(1~3)</param>
     void SetActiceEnemyBattleUI(int EnemyAmount)
     {
-        if (isEnemyBattleUI == true)
+        if (openEnemyBattleUI == true)
         {
             for (int i = 0; i < EnemyAmount; i++)
             {
